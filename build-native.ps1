39432f5e2d9150f8cd3fcd718afa9547bef4c8d3
Summpot <summpot@outlook.com>
2023-08-12 20:19:39 +0800

    update

--- build-native.ps1
+++ build-native.ps1
@@ -8,7 +8,20 @@ $ErrorActionPreference = "Continue"
 $CMakeBuildDir = "cmake-build-$RID"
 New-Item -Path $CMakeBuildDir -ItemType Directory -Force
 $OS, $Arch = $RID -split "-"
+$SystemName = switch ($OS) {
+    "linux" { "Linux" }
+    "osx" { "Darwin" }
+    "win" { "Windows" }
+    Default { "Generic" }
+}
+
+$SystemProcessor = switch ($Arch) {
+    "x64" { "x64" }
+    "x86" { "x86" }
+    "arm64" { "aarch64" }
+    Default {}
+}
 Push-Location $CMakeBuildDir
-& cmake -GNinja "../tree-sitter"
+& cmake -GNinja -DCMAKE_SYSTEM_NAME="$SystemName" -DCMAKE_SYSTEM_PROCESSOR="$SystemProcessor" "../tree-sitter"
 & cmake --build .
 Pop-Location
