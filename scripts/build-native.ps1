4c32753b6391ba7294333a025c6151fb5d3818dd
Summpot <summpot@outlook.com>
2023-08-13 23:30:46 +0800

    Update

--- scripts/build-native.ps1
+++ scripts/build-native.ps1
@@ -5,9 +5,13 @@ Param(
     [string] $CMakeBuildDir,
     [string] $OutputDir
 )
-
+if (-not (Test-Path $OutputDir)) {
+    New-Item -Path $OutputDir -ItemType Directory -Force | Out-Null
+}
 $BuildDir = Join-Path $CMakeBuildDir "build-$RID"
-New-Item -Path $BuildDir -ItemType Directory -Force | Out-Null
+if (-not (Test-Path $BuildDir)) {
+    New-Item -Path $BuildDir -ItemType Directory -Force | Out-Null
+}
 $OS, $Arch = $RID -split "-"
 $SystemName = switch ($OS) {
     "linux" { "Linux" }
@@ -26,9 +30,12 @@ Push-Location $BuildDir
 & cmake -GNinja -DCMAKE_SYSTEM_NAME="$SystemName" -DCMAKE_SYSTEM_PROCESSOR="$SystemProcessor" "$ProjectDir"
 & cmake --build .
 Pop-Location
+if (-not (Test-Path "$OutputDir/$RID")) {
+    New-Item -Path "$OutputDir/$RID" -ItemType Directory -Force | Out-Null
+}
 switch ($OS) {
-    "linux" { Copy-Item "$BuildDir/libtree-sitter.so" "$OutputDir/libtree-sitter.so" }
-    "osx" { Copy-Item "$BuildDir/libtree-sitter.dylib" "$OutputDir/libtree-sitter.dylib" }
-    "win" { Copy-Item "$BuildDir/tree-sitter.dll" "$OutputDir/libtree-sitter.dll" }
+    "linux" { Copy-Item "$BuildDir/libtree-sitter.so" "$OutputDir/$RID/libtree-sitter.so" }
+    "osx" { Copy-Item "$BuildDir/libtree-sitter.dylib" "$OutputDir/$RID/libtree-sitter.dylib" }
+    "win" { Copy-Item "$BuildDir/tree-sitter.dll" "$OutputDir/$RID/libtree-sitter.dll" }
     Default {}
 }
