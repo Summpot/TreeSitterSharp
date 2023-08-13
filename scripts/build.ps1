6a95249626f06bae60797534c79fb42ddef1d5ae
Summpot <summpot@outlook.com>
2023-08-13 23:18:15 +0800

    Update

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,9 +6,9 @@ Param(
     [Parameter(Mandatory = $true)][string] $BuildDir
 )
 $ProjectDir = Resolve-Path (Join-Path $PWD $ProjectDir)
-$Version = & "$PSScriptRoot/Get-GitVersion.ps1"
+$Version = ./Get-GitVersion.ps1
 $NuspecsDir = Join-Path $BuildDir "nuspecs"
 $CMakeBuildDir = Join-Path $BuildDir "cmake-build"
-& "$PSScriptRoot/Build-Native.ps1" -RID $RID -CMakeBuildDir $CMakeBuildDir -OutputDir $NuspecsDir -ProjectDir $ProjectDir
-& "$PSScriptRoot/Generate-MainNuspec.ps1" -OutputDir $NuspecsDir -Version $Version -LibraryName $LibraryName
-& "$PSScriptRoot/Generate-Nuspec.ps1" -RID $RID -OutputDir $NuspecsDir -Version $Version -LibraryName $LibraryName
+./Build-Native.ps1 -RID $RID -CMakeBuildDir $CMakeBuildDir -OutputDir $NuspecsDir -ProjectDir $ProjectDir
+./Generate-MainNuspec.ps1 -OutputDir $NuspecsDir -Version $Version -LibraryName $LibraryName
+./Generate-Nuspec.ps1 -RID $RID -OutputDir $NuspecsDir -Version $Version -LibraryName $LibraryName
