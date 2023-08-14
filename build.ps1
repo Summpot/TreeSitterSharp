e4493db921f7ada606f1a53210c034548510c515
Summpot <summpot@outlook.com>
2023-08-14 19:02:11 +0800

    Update

--- build.ps1
+++ build.ps1
@@ -11,7 +11,7 @@ $ParsersDir = Join-Path $NativeDir "parsers"
 New-MainNuspec -OutputDir $NuspecsDir -Version $Version -ProjectName "tree-sitter"
 New-Nuspec -RID $RID -OutputDir $NuspecsDir -Version $Version -ProjectName "tree-sitter"
 Build-CMakeProject -RID $RID -CMakeBuildDir $CMakeBuildDir -OutputDir $NuspecsDir -ProjectDir $TreeSitterDir
-Get-ChildItem $ParsersDir | ForEach-Object { 
+Get-ChildItem -Path $ParsersDir | ForEach-Object { 
     Build-NodeGypProject -ProjectDir $_ -OutputDir $NuspecsDir -RID $RID 
     New-MainNuspec -OutputDir $NuspecsDir -Version $Version -ProjectName $_.Name
     New-Nuspec -RID $RID -OutputDir $NuspecsDir -Version $Version -ProjectName $_.Name
