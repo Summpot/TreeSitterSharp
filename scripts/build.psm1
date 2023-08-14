2b99e806e9da5bfd217208070a69dec18e757621
Summpot <summpot@outlook.com>
2023-08-14 15:56:17 +0800

    Update

--- scripts/build.psm1
+++ scripts/build.psm1
@@ -23,34 +23,34 @@ function New-MainNuspec {
     $NuspecName = "lib$ProjectName.nuspec"
     $NuspecPath = Join-Path $OutputDir $NuspecName
     $Nuspec = @"
-    <?xml version="1.0" encoding="utf-8"?>
-    <package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
-      <metadata minClientVersion="2.12">
-        <id>lib$ProjectName</id>
-        <version>$Version</version>
-        <authors>Summpot</authors>
-        <owners>Summpot</owners>
-        <requireLicenseAcceptance>false</requireLicenseAcceptance>
-        <license type="expression">MIT</license>
-        <projectUrl>https://github.com/Summpot/tree-sitter</projectUrl>
-        <description>Multi-platform native library for lib$ProjectName.</description>
-        <copyright>Copyright (c) 2023 Summpot</copyright>
-        <repository type="git" url="https://github.com/tree-sitter/tree-sitter" branch="master" />
-        <dependencies>
-          <group targetFramework=".NETStandard2.0" >
-            <dependency id="lib$ProjectName.runtime.linux-arm64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.linux-x64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.osx-arm64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.osx-x64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.win-arm64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.win-x64" version="$Version" />
-            <dependency id="lib$ProjectName.runtime.win-x86" version="$Version" />
-          </group>
-        </dependencies>
-      </metadata><files>
-        <file src="LICENSE.TXT" target="LICENSE.TXT" />
-      </files>
-    </package>
+<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
+  <metadata minClientVersion="2.12">
+    <id>lib$ProjectName</id>
+    <version>$Version</version>
+    <authors>Summpot</authors>
+    <owners>Summpot</owners>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <license type="expression">MIT</license>
+    <projectUrl>https://github.com/Summpot/tree-sitter</projectUrl>
+    <description>Multi-platform native library for lib$ProjectName.</description>
+    <copyright>Copyright (c) 2023 Summpot</copyright>
+    <repository type="git" url="https://github.com/tree-sitter/tree-sitter" branch="master" />
+    <dependencies>
+      <group targetFramework=".NETStandard2.0" >
+        <dependency id="lib$ProjectName.runtime.linux-arm64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.linux-x64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.osx-arm64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.osx-x64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.win-arm64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.win-x64" version="$Version" />
+        <dependency id="lib$ProjectName.runtime.win-x86" version="$Version" />
+      </group>
+    </dependencies>
+  </metadata><files>
+    <file src="LICENSE.TXT" target="LICENSE.TXT" />
+  </files>
+</package>
 "@
     Out-File -InputObject $Nuspec -FilePath $NuspecPath -Force
 }
@@ -82,25 +82,25 @@ function New-Nuspec {
     $NuspecName = "lib$ProjectName.runtime.$RID.nuspec"
     $NuspecPath = Join-Path $OutputDir $NuspecName
     $Nuspec = @"  
-      <?xml version="1.0" encoding="utf-8"?>
-      <package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
-        <metadata minClientVersion="2.12">
-          <id>lib$ProjectName.runtime.$RID</id>
-          <version>$version</version>
-          <authors>Summpot</authors>
-          <owners>Summpot</owners>
-          <requireLicenseAcceptance>false</requireLicenseAcceptance>
-          <license type="expression">MIT</license>
-          <projectUrl>https://github.com/Summpot/tree-sitter</projectUrl>
-          <description>linux arm64 native library for libtree-sitter.</description>
-          <copyright>Copyright (c) 2023 Summpot</copyright>
-          <repository type="git" url="https://github.com/Summpot/tree-sitter" branch="master" />
-        </metadata>
-        <files>
-          <file src="LICENSE.TXT" target="LICENSE.TXT" />
-          <file src="$RID/$SourceTargetName$extension" target="runtimes\$RID\native\lib$ProjectName$extension" />
-        </files>
-      </package>
+<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd">
+  <metadata minClientVersion="2.12">
+    <id>lib$ProjectName.runtime.$RID</id>
+    <version>$version</version>
+    <authors>Summpot</authors>
+    <owners>Summpot</owners>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <license type="expression">MIT</license>
+    <projectUrl>https://github.com/Summpot/tree-sitter</projectUrl>
+    <description>linux arm64 native library for libtree-sitter.</description>
+    <copyright>Copyright (c) 2023 Summpot</copyright>
+    <repository type="git" url="https://github.com/Summpot/tree-sitter" branch="master" />
+  </metadata>
+  <files>
+    <file src="LICENSE.TXT" target="LICENSE.TXT" />
+    <file src="$RID/$SourceTargetName$extension" target="runtimes\$RID\native\lib$ProjectName$extension" />
+  </files>
+</package>
 "@
     Out-File -InputObject $Nuspec -FilePath $NuspecPath -Force
 }
