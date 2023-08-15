79b19fd480cc0f4da4f5ccab49e60c4525bf347a
Summpot <summpot@outlook.com>
2023-08-15 21:54:40 +0800

    Update

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+Param(
+    [switch] $Pack
+)
 
 function Build-MesonProject {
     Param(
@@ -11,6 +14,7 @@ function Build-MesonProject {
         "linux" { ".so" }
         Default {}
     }
+
     $SourceDir = Join-Path "native/sources" $ProjectName
     $BuildDir = Join-Path $SourceDir "build-$RID"
     $CrossfilePath = "native/crossfiles/$RID.txt"
@@ -21,15 +25,15 @@ function Build-MesonProject {
     $TargetItem = Get-ChildItem -Path $BuildDir -Filter "*$Extensin" -File
     $TargetDir = Join-Path $NuspecsDir $RID
     $TargetPath = Join-Path $TargetDir "$($TargetItem.Name)"
-    New-Item -Path $TargetDir -ItemType Directory -ErrorAction Continue
+    New-Item -Path $TargetDir -ItemType Directory -ErrorAction SilentlyContinue
     Copy-Item $TargetItem $TargetPath
     $NuspecPath = Join-Path $NuspecsDir "lib$ProjectName.runtime.$RID.nuspec"
-    & nuget pack $NuspecPath
+    if ($Pack) {
+        & nuget pack $NuspecPath
+    }
 }
 
 $ErrorActionPreference = "Stop"
-
-
 $ProjectNames = @("tree-sitter", "tree-sitter-c", "tree-sitter-cpp", "tree-sitter-lua")
 if ($IsWindows) {
     $RIDS = @("win-x86", "win-x64", "win-arm64")
@@ -43,10 +47,32 @@ elseif ($IsMacOS) {
 else {
     Write-Error "System is not supported."
 }
+$GitVersionInfo = & dotnet-gitversion.exe | ConvertFrom-Json
+$Version = $GitVersionInfo.FullSemVer
+$Nuspecs = Get-ChildItem -Path "native/nuspecs" -Filter "*.nuspec"  -File
+foreach ($Nuspec in $Nuspecs) {
+    $xml = [System.Xml.Linq.XDocument]::Load($Nuspec.FullName)
+    $ns = $xml.Root.Name.Namespace
+    $versionNodes = $xml.Descendants($ns + "version")
+    foreach ($versionNode in $versionNodes) {
+        $versionNode.SetValue($Version)
+    }
+    $dependencyNodes = $xml.Descendants($ns + "dependency")
+    foreach ($dependencyNode in $dependencyNodes) {
+        $versionAttribute = $dependencyNode.Attribute("version")
+        if ($null -ne $versionAttribute) {
+            $versionAttribute.SetValue($Version)
+        }
+    }
+    $xml.Save($Nuspec.FullName)
+}
 foreach ($ProjectName in $ProjectNames) {
     foreach ($RID in $RIDS) {
         Build-MesonProject -RID $RID -ProjectName $ProjectName
-        & nuget pack "native/nuspecs/lib$ProjectName.nuspec"
+        if ($Pack) {
+            & nuget pack "native/nuspecs/lib$ProjectName.nuspec"
+        }
+        
     }  
 }
 
